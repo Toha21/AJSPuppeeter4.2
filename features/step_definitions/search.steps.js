0c6bf73e1f6762859f19fc106e939cc4f9a4dd61
Антон Гаврюхин <ah21@mail.ru>
2023-03-14 14:17:14 +0300

    modification

--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -35,25 +35,25 @@ Given("user is on page", async function () {
   });
 });
 
-When("user choose day and movie", async function (int1) {
+When("user choose day and movie", async function () {
   //выбор дня по дате и сеанса
   return await selectDateTime(
     this.page,
-    `nav.page-nav > a:nth-child(${int1})`,
+    `nav.page-nav > a:nth-child(${2})`,
     movieTime
   );
 });
 
-When("select row and seat", async function (int1, int2) {
+When("select row and seat", async function () {
   //выбор ряда и 1 места
-  return await orderTickets(this.page, int1, int2);
+  return await orderTickets(this.page, 7, 3);
 });
 
 When(
-  "select row and two seats",
-  async function (int1, int2, int3) {
+  "select row and seats",
+  async function () {
     //выбор ряда и нескольких мест
-    return await orderTickets(this.page, int1, int2, int3);
+    return await orderTickets(this.page, 7, 7, 8);
   }
 );
 
